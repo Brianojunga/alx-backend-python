b3ed86ecef5b359b595fedce3e01f784bd915d15
Brianojunga <brianojunga3@gmail.com>
2025-11-08 17:21:26 +0300

    Add a query and param for context manager implementation

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(self.query, self.params)
+        return self.cursor.fetchall()
+    
+    def __exit__(self, type, value, traceback):
+        if type:
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
