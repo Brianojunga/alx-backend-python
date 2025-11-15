f164d207f5809252575094727e9f0d54dc2c1c78
Brianojunga <brianojunga3@gmail.com>
2025-11-15 20:20:58 +0300

    Add viewsets for creations and listings

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, filter
+from rest_framework.response import Response
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer
 
 # Create your views here.
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    # Example: override create to automatically set sender
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender_id=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ConversationViewset(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    # Optional: add filtering by participants
+    filter_backends = [filter.SearchFilter]
+    search_fields = ['participants_id__first_name', 'participants_id__last_name']
