08f005a150173e442a4cea7569b25fe524e760d6
Brianojunga <brianojunga3@gmail.com>
2025-11-16 20:28:50 +0300

    Mock calling http request

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -21,6 +22,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), repr(expected_key))
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test that get_json returns expected payload."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch("utils.requests.get", return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            # Test that requests.get was called once with test_url
+            mock_get.assert_called_once_with(test_url)
+            # Test that the output of get_json is equal to test_payload
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
