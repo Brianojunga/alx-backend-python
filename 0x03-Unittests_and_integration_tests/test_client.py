59e21c32c969f90ff05ea705297f577be2010ffe
Brianojunga <brianojunga3@gmail.com>
2025-11-16 21:59:19 +0300

    Mock test for repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,6 +28,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, {"payload": True})
 
+    def test_public_repos_url(self):
+        """Test GithubOrgClient._public_repos_url"""
+        # Mocked payload
+        mock_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+
+        with patch("client.GithubOrgClient.org", return_value=mock_payload):
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+
+            self.assertEqual(result, mock_payload["repos_url"])
+
 
 if __name__ == "__main__":
     unittest.main()
