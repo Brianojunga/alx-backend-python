f987f0615acf9d120d9169ebb551cf44bfcf59b9
Brianojunga <brianojunga3@gmail.com>
2025-11-16 23:45:02 +0300

    implementation for testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,10 +31,16 @@ class TestGithubOrgClient(unittest.TestCase):
             "repos_url": "https://api.github.com/orgs/google/repos"
         }
 
-        with patch("client.GithubOrgClient.org", return_value=mock_payload):
+        # Patch the 'org' property with PropertyMock
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = mock_payload
             client = GithubOrgClient("google")
             result = client._public_repos_url
-            self.assertEqual(result, mock_payload["repos_url"])
+
+        assert result == mock_payload["repos_url"]
 
     @patch("client.get_json")
     def test_public_repos(self, mock_get_json):
