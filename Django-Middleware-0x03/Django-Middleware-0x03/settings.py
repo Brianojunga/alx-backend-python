7b868e57f3a4434dee0686dc9b407f2fd03036e1
Brianojunga <brianojunga3@gmail.com>
2025-11-23 23:51:58 +0300

    Add middlewares

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "chats.middleware.OffensiveLanguageMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.OffensiveLanguageMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
     "chats.middleware.RolepermissionMiddleware",
